2d3e0b31a31cf0bfd29c7fad7d4dc19228624a05
潘志清 <82391775+pzq123456@users.noreply.github.com>
2023-04-14 22:30:49 +0800

    123

---
+++ EnvSetUp/dlDS.sh
@@ -0,0 +1,7 @@
+wget https://gitlab.com/pzq123456/train/-/archive/master/train-master.tar.gz
+# 解压
+tar -zxvf train-master.tar.gz
+
+wget https://gitlab.com/pzq123456/test/-/archive/master/test-master.tar.gz
+# 解压
+tar -zxvf test-master.tar.gz
